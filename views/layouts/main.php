0c9d0438624d7a60b436885a712b7bb50e2db47f
Max Matiukhin <matuhinmax@mail.ru>
2017-02-13 23:35:13 +0200

    Добавил пункт штаты в меню.

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -55,10 +55,15 @@ AppAsset::register($this);
                 . Html::a('<i class="fa fa-map-marker"></i> My addresses', ['/address/'], ['class' => 'profile-link'])
                 . '</li>'
                 .'<li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-wrench"></i> Configuration <span class="caret"></span></a>
-          <ul class="dropdown-menu">'
-                . Html::a('Tariffs', ['/tariff/'], ['class' => 'profile-link'])
-                .'</ul>'
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-wrench"></i> Configuration <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li>'
+                            . Html::a('Tariffs', ['/tariff/'], ['class' => 'profile-link'])
+                        .'</li>
+                        <li>'
+                            . Html::a('State and tax', ['/state/'], ['class' => 'profile-link'])
+                        .'</li>'
+                    .'</ul>'
                 .'</li>'.
 
                                   '<li>'
