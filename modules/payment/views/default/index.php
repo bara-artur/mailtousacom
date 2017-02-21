a5013f6bc2df600c15596a416e1e68bf098eee60
Max Matiukhin <matuhinmax@mail.ru>
2017-02-21 22:10:08 +0200

    Сделал слияние с 013-2. Для отображения фильтра.

--- modules/payment/views/default/index.php
+++ modules/payment/views/default/index.php
@@ -1,56 +1,54 @@
 <?php
-
 use yii\helpers\Html;
 use yii\grid\GridView;
-
+use app\modules\payment\models\PaymentsList;
 /* @var $this yii\web\View */
 /* @var $searchModel app\modules\payment\models\PaymentSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
-
 $this->title = 'Personal Payments Lists';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="payments-list-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-    <?= $this->render('paymentFilterForm', ['model' => $filterForm]);?>
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-            ['attribute'=> 'status',
-                'content' => function($data){
-                    return $data::statusText($data->status);
-                },
-                'filter' => PaymentsList::getTextStatus(),
-              ],
-            ['attribute'=> 'type',
-                'content' => function($data){
-                    return $data::statusPayText($data->status);
-                },
-                'filter' => PaymentsList::getPayStatus(),
-            ],
-            [
-                'attribute' => 'price',
-                'format'=>['decimal',2]
-            ],
-                [
-                    'attribute' => 'qst',
-                    'format'=>['decimal',2]
-                ],
-            [
-                'attribute' => 'gst',
-                'format'=>['decimal',2]
-            ],
-            [
-                'attribute'=> 'pay_time',
-                'content' => function($data){
-                    if ($data->pay_time  == 0 ) return 'Expected...';
-                    else return date(\Yii::$app->params['data_time_format_php'],$data->pay_time);
-                },
-            ],
-        ],
-    ]); ?>
+  <h1><?= Html::encode($this->title) ?></h1>
+  <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+  <?= $this->render('paymentFilterForm', ['model' => $filterForm]);?>
+  <?= GridView::widget([
+    'dataProvider' => $dataProvider,
+    'filterModel' => $searchModel,
+    'columns' => [
+      ['class' => 'yii\grid\SerialColumn'],
+      ['attribute'=> 'status',
+        'content' => function($data){
+          return $data::statusText($data->status);
+        },
+        'filter' => PaymentsList::getTextStatus(),
+      ],
+      ['attribute'=> 'type',
+        'content' => function($data){
+          return $data::statusPayText($data->status);
+        },
+        'filter' => PaymentsList::getPayStatus(),
+      ],
+      [
+        'attribute' => 'price',
+        'format'=>['decimal',2]
+      ],
+      [
+        'attribute' => 'qst',
+        'format'=>['decimal',2]
+      ],
+      [
+        'attribute' => 'gst',
+        'format'=>['decimal',2]
+      ],
+      [
+        'attribute'=> 'pay_time',
+        'content' => function($data){
+          if ($data->pay_time  == 0 ) return 'Expected...';
+          else return date(\Yii::$app->params['data_time_format_php'],$data->pay_time);
+        },
+      ],
+    ],
+  ]); ?>
 </div>
--- views/site/index.php
+++ views/site/index.php
@@ -5,88 +5,85 @@ use yii\grid\GridView;
 use app\modules\payment\models\PaymentsList;
 use yii\jui\DatePicker;
 use kartik\daterange\DateRangePicker;
-
 /* @var $this yii\web\View */
 $this->title = 'Shipping to USA and Canada';
 ?>
-<div class="site-index">
-    <?= skinka\widgets\gritter\AlertGritterWidget::widget() ?>
-    <?php if (Yii::$app->session->hasFlash('signup-success')) { ?> <p> <?= Yii::$app->session->getFlash('signup-success');  ?> </p>  <?php } ?>
-    <?php if (Yii::$app->session->hasFlash('reset-success')) { ?> <p> <?= Yii::$app->session->getFlash('reset-success');  ?> </p>  <?php } ?>
+    <div class="site-index">
+        <?= skinka\widgets\gritter\AlertGritterWidget::widget() ?>
+        <?php if (Yii::$app->session->hasFlash('signup-success')) { ?> <p> <?= Yii::$app->session->getFlash('signup-success');  ?> </p>  <?php } ?>
+        <?php if (Yii::$app->session->hasFlash('reset-success')) { ?> <p> <?= Yii::$app->session->getFlash('reset-success');  ?> </p>  <?php } ?>
 
-    <?= UserWidget::widget() ?>
+        <?= UserWidget::widget() ?>
 
-</div>
+    </div>
 <?php
 if (!Yii::$app->user->isGuest) {
-?>
-<p>
-    <?= Html::a('Create Order', ['/order/create'], ['class' => 'btn btn-success']) ?>
-</p>
-<div>
-    <?= GridView::widget([
-        'dataProvider' => $orders,
-        'columns' => [
+    ?>
+    <p>
+        <?= Html::a('Create Order', ['/address/create-order-billing'], ['class' => 'btn btn-success']) ?>
+    </p>
+    <div>
+        <?= $this->render('orderFilterForm', ['model' => $filterForm]);?>
+        <?= GridView::widget([
+          'dataProvider' => $orders,
+          'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
-
             'id',
             ['attribute'=> 'order_status',
-                'content' => function($data){
-                    return $data::orderStatusText($data->order_status);
-                },
+              'content' => function($data){
+                  return $data::orderStatusText($data->order_status);
+              },
             ],
             ['attribute'=> 'created_at',
-                'content'=> function($data){
-                    if ($data->created_at == 0) return '-';
-                    else return date(\Yii::$app->params['data_time_format_php'],$data->created_at);
-                },
-
-            'format' => 'raw',
-          ],
+              'content'=> function($data){
+                  if ($data->created_at == 0) return '-';
+                  else return date(\Yii::$app->params['data_time_format_php'],$data->created_at);
+              },
+              'format' => 'raw',
+            ],
             ['attribute'=> 'transport_data',
-            'content'=> function($data){
-                if ($data->transport_data == 0) return '-';
-                else return date(\Yii::$app->params['data_time_format_php'],$data->transport_data);
-            }],
+              'content'=> function($data){
+                  if ($data->transport_data == 0) return '-';
+                  else return date(\Yii::$app->params['data_time_format_php'],$data->transport_data);
+              }],
             ['attribute'=> 'payment_type',
-                'content' => function($data){
-                    return PaymentsList::getPayStatus()[$data->payment_type];
-                },
-                'filter' => PaymentsList::getPayStatus(),
+              'content' => function($data){
+                  return PaymentsList::getPayStatus()[$data->payment_type];
+              },
+              'filter' => PaymentsList::getPayStatus(),
             ],
             ['attribute'=> 'payment_state',
-            'content' => function($data){
-                return PaymentsList::getTextStatus()[$data->payment_state];
-            },
+              'content' => function($data){
+                  return PaymentsList::getTextStatus()[$data->payment_state];
+              },
             ],
             [
-                'attribute' => 'price',
-                'content'=> function($data){
-                    if ($data->price == 0) return '-';
-                    else return $data->price;
-                },
-                'format'=>['decimal',2]
+              'attribute' => 'price',
+              'content'=> function($data){
+                  if ($data->price == 0) return '-';
+                  else return $data->price;
+              },
+              'format'=>['decimal',2]
             ],
             [
-                'attribute' => 'qst',
-                'content'=> function($data){
-                    if ($data->qst == 0) return '-';
-                    else return $data->qst;
-                },
-                'format'=>['decimal',2]
+              'attribute' => 'qst',
+              'content'=> function($data){
+                  if ($data->qst == 0) return '-';
+                  else return $data->qst;
+              },
+              'format'=>['decimal',2]
             ],
             [
-                'attribute' => 'gst',
-                'content'=> function($data){
-                    if ($data->gst == 0) return '-';
-                    else return $data->gst;
-                },
-                'format'=>['decimal',2]
+              'attribute' => 'gst',
+              'content'=> function($data){
+                  if ($data->gst == 0) return '-';
+                  else return $data->gst;
+              },
+              'format'=>['decimal',2]
             ],
-            // 'order_status',
-            // 'created_at',
-            // 'transport_data',
-
+              // 'order_status',
+              // 'created_at',
+              // 'transport_data',
             ['content' => function($data){
                 switch ($data->order_status) {
                     case '0' : return  Html::a('Update Order', ['/orderInclude/create-order/'.$data->id], ['class' => 'btn btn-success']); break;
@@ -95,10 +92,8 @@ if (!Yii::$app->user->isGuest) {
                     case '3' : return Html::a('View', ['/order/view/'.$data->id], ['class' => 'btn btn-info']);break;
                     default: return "Unknown status - ".$data->order_status;
                 }
-                }],
-
-        ],
-    ]); ?>
-</div>
+            }],
+          ],
+        ]); ?>
+    </div>
 <?php }?>
-
