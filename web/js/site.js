a9dda18d82c79a854c7ab328fe8af5f1aa6772b1
Anatoliy Galyuk <tolikybm@gmail.com>
2017-03-27 21:37:26 +0300

    Кнопка signUP и чекбокс соглашения при регистрации

--- web/js/site.js
+++ web/js/site.js
@@ -19,14 +19,14 @@ $(document).ready(function() {
     }
   });
 
-  $("#w0 button[name='signup-button']" ).prop("disabled",true);;
+  $("button[name='signup-button']" ).prop("disabled",true);;
   $("input[name='I_accept']" ).on( "change", function() {
     if($("input[name='I_accept']").prop("checked")) {
-      $("#w0 button[name='signup-button']").prop("disabled",false);
+      $("button[name='signup-button']").prop("disabled",false);
       //$("input[name='I_accept']").prop('checked', true);
     }
     else{
-      $("#w0 button[name='signup-button']").prop("disabled",true);
+      $("button[name='signup-button']").prop("disabled",true);
       //$("input[name='I_accept']").prop('checked', false);
     }
   });
