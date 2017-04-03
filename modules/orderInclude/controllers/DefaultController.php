044fb5d3bf012f04b75c69c0733c6c4b27d3ba0c
Anatoliy Galyuk <tolikybm@gmail.com>
2017-04-03 16:05:09 +0300

    Вывод уведомления о вводе неправильных символов. Один раз для каждого поля текущей страницы

--- modules/orderInclude/controllers/DefaultController.php
+++ modules/orderInclude/controllers/DefaultController.php
@@ -127,7 +127,7 @@ class DefaultController extends Controller
     ];
 
     foreach($parcels as $parcel){
-      if(!$users_parcel[$parcel->user_id]){
+      if(!isset($users_parcel[$parcel->user_id])){
         $users_parcel[$parcel->user_id]=[];
         $total['weight_by_user'][$parcel->user_id]=0;
         $users[]=$parcel->user_id;
