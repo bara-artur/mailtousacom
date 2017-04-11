bce9487fc9d210979983951e699a006e8f945917
Anatoliy Galyuk <tolikybm@gmail.com>
2017-04-12 02:33:13 +0300

    Начало парсинга сайта транспортных компаний

--- modules/cron/controllers/DefaultController.php
+++ modules/cron/controllers/DefaultController.php
@@ -6,6 +6,7 @@ use Yii;
 use yii\web\Controller;
 use app\modules\orderElement\models\OrderElement;
 use app\modules\orderElement\models\OrderElementSearch;
+use keltstr\simplehtmldom\SimpleHTMLDom as SHD;
 
 /**
  * Default controller for the `cron` module
@@ -23,6 +24,22 @@ class DefaultController extends Controller
         $parcel->cron_refresh = time();
         $parcel->save();
       }
+
+
+      $html = SHD::file_get_html('https://www.fedex.com/apps/fedextrack/?action=track&tracknumbers=786115185080',null,null,1,1);
+      var_dump(count($html->find('div.statusChevron_area')));
+      foreach($html->find('.statusChevron_key_status') as $element)
+        var_dump('value = '.$element->value);
+
+/*      fore0ach ($data as $i=>$parcel){
+        $html = SHD::file_get_html('https://www.fedex.com/apps/fedextrack/?action=track&tracknumbers='.$parcel->track_number,null,null,1,1);
+        if ($i==0) var_dump();
+
+      //$html = file_get_contents('http://www.google.com/');
+        var_dump(count($html->find('.statusChevron_key_status')));
+      foreach($html->find('.statusChevron_key_status') as $element)
+        var_dump('value = '.$element->value);
+      }*/
       return $this->render('index', [
        'data' => $data,
       ]);
