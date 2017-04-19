4427a06241916f45bc4536f1aa8e92ad91815ac1
Max Matiukhin <matuhinmax@mail.ru>
2017-04-19 23:15:54 +0300

    В конфиг ввел параметр часового пояса.

--- config/web.php
+++ config/web.php
@@ -9,6 +9,7 @@ $config = [
   'id' => 'basic',
   'basePath' => dirname(__DIR__),
   'bootstrap' => ['log'],
+  'timeZone' => 'Etc/GMT-5',
   'components' => [
     'request' => [
       'baseUrl' => '',
