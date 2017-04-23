98e4f8b13711a2b897cbf967821282c0d1a691c7
Anatoliy Galyuk <tolikybm@gmail.com>
2017-04-23 03:13:18 +0300

    Минимальная длина поля Zip В адресе посылки 5 символов

--- modules/orderElement/models/OrderElement.php
+++ modules/orderElement/models/OrderElement.php
@@ -85,6 +85,7 @@ class OrderElement extends \yii\db\ActiveRecord
             [['first_name', 'last_name','company_name', 'adress_1','city', 'zip', 'state'], 'required'],
             [['first_name', 'last_name', 'city', 'zip', 'phone', 'state'], 'string', 'max' => 60],
             [['company_name'], 'string', 'max' => 128],
+            [['zip'], 'string', 'min' => 5],
             [['track_number'], 'string'],
             [['price','qst','gst'],'double'],
             [['weight'], 'double'],
