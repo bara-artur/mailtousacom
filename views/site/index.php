d40f99c7cd6355f5d1bb4aee2d88c6db5c72c173
Anatoliy <tolikybm@gmail.com>
2017-07-17 23:05:48 +0300

    Исправление вывода на главной

--- views/site/index.php
+++ views/site/index.php
@@ -186,7 +186,7 @@ $this->title = 'Shipping to USA and Canada';
                     }
                   }
                 ],
-                ['attribute'=> 'id',
+                ['attribute'=> 'user_id',
                   'visible' => (($showTable->showUserID)&&($admin==1)),
                   'content'=> function($data){
                       return $data->user_id+750;
